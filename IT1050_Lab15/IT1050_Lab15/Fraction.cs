ef815974b0df1d2f099fd7003a329b401503fc68
Jeank9753 <51294170+Jeank9753@users.noreply.github.com>
2021-12-11 13:00:02 -0500

    Add files via upload

---
+++ IT1050_Lab15/IT1050_Lab15/Fraction.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT1050_Lab15
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+
+        }
+
+        public void Multiply(int numerator, int denominator)
+        {
+            this.numerator = this.numerator * numerator;
+            this.denominator = this.denominator * denominator;
+        }
+
+        public void Multiply(int wholeNumber)
+        {
+            this.numerator = this.numerator * wholeNumber;
+        }
+
+        public void Multiply(Fraction fraction)
+        {
+            this.numerator = this.numerator * fraction.numerator;
+            this.denominator = this.denominator * fraction.denominator;
+        }
+
+        public void Divide(int numerator, int denominator)
+        {
+
+        }
+
+        public void Divide(int wholeNumber)
+        {
+            
+        }
+
+        public void Divide(Fraction fraction)
+        {
+
+        }
+
+        public void Add(int numerator, int denominator)
+        {
+
+        }
+
+        public void Add(int wholeNumber)
+        {
+
+        }
+
+        public void Add(Fraction fraction)
+        {
+
+        }
+
+        public void Subtract(int numerator, int denominator)
+        {
+
+        }
+
+        public void Subtract(int wholeNumber)
+        {
+
+        }
+
+        public void Subtract(Fraction fraction)
+        {
+
+        }
+
+        public string DisplayFraction()
+        {
+
+        }
+    }
+}
---
+++ IT1050_Lab15/IT1050_Lab15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IT1050_Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(5, 6);
+            Fraction f2 = new Fraction(3, 4);
+
+            f1.Multiply(3, 2);
+            f2.Add(1, 2);
+            f2.Subtract(1);
+            f1.Subtract(f2);
+            f1.Divide(3);
+
+            Console.WriteLine("Fraction 1 is now: " + f1.DisplayFraction());
+            Console.WriteLine("Fraction 2 is now: " + f2.DisplayFraction());
+
+            Console.ReadKey();
+        }
+    }
+}
